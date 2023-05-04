86dc1be0fccfd05cd38c05185127335848efaa65
Replit user <>
2023-05-04 04:03:53 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -61,16 +61,15 @@ List* get_adj_nodes(Node* n)
   List* list=createList();
   int move;
   
-
   for(int i = 0; i < 9; i++)
   {
     for(int k = 0; k < 9; k++)
     {
-      if(n->sudo[i][k] == '\0')
+      if(n->sudo[i][k] == 0)
       {
         Node *adj_n = (Node*) malloc(sizeof(Node));
         
-        adj_n->sudo = copy(n);
+        adj_n->sudo = copy(n->sudo);
         adj_n->sudo[i][k] = move;
         pushBack(list, adj_n);
       }
