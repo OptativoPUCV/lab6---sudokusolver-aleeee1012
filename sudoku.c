8086ef2deb6dcb2b5df471ea5e27b8fb619a1ac6
Replit user <>
2023-05-06 23:34:56 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -80,7 +80,6 @@ int is_valid(Node * n)
   return 1;
 }
 
-
 List* get_adj_nodes(Node* n)
 {
   List *list = createList();
@@ -107,8 +106,16 @@ List* get_adj_nodes(Node* n)
   return list;
 }
 
-int is_final(Node* n){
-    return 0;
+int is_final(Node* n)
+{
+  for(int i = 0; i < 9; i++)
+  {
+    for(int k = 0; k < 9; k++)
+    {
+      if(n->sudo[i][k] == 0) return 0;
+    }
+  }
+  return 1;
 }
 
 Node* DFS(Node* initial, int* cont){
